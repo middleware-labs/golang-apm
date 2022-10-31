f2a391c014fd8514f2ea14d4ae6661c4de622737
meealmathur23 <meenalmathur23@gmail.com>
2022-10-31 14:35:34 +0530

    minor updates

--- tracker/config.go
+++ tracker/config.go
@@ -62,7 +62,7 @@ func newConfig(opts ...Options) *config {
 		}
 	}
 
-	c.host = getHostValue("MW_AGENT_SERVICE", "localhost:4320")
+	c.host = getHostValue("MW_AGENT_SERVICE", "localhost:9319")
 
 	if c.projectName == "" {
 		if v, ok := c.settings["projectName"]; ok {

28eefac2b1d6aa7d2f9fd3cbfc419f33481fb117
meealmathur23 <meenalmathur23@gmail.com>
2022-11-11 11:30:24 +0530

    bug fixes

--- tracker/tracker.go
+++ tracker/tracker.go
@@ -1,9 +1,5 @@
 package tracker
 
-import (
-	"log"
-)
-
 func Track(opts ...Options) (*Config, error) {
 	c := newConfig(opts...)
 	if c.pauseTraces == false {
@@ -11,11 +7,7 @@ func Track(opts ...Options) (*Config, error) {
 	}
 	if c.pauseMetrics == false {
 		handler := Tracer{}
-		err := handler.init(c)
-		if err != nil {
-			log.Fatalf("failed to create the collector exporter: %v", err)
-			return nil, err
-		}
+		go handler.init(c)
 	}
 	return c, nil
 }
